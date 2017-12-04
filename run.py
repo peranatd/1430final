709856b29b232e9a6dd263ae3996f0bee816f3bd
Peranat Dayananda <pdayanan@cs.brown.edu>
2017-12-04 18:09:02 -0500

    Implement lazy data loading in Gaze, use entire dataset. Remove invalid data paths.

--- run.py
+++ run.py
@@ -10,9 +10,10 @@ from tensorpack.utils.gpu import get_nr_gpu
 from tensorpack.dataflow.base import RNGDataFlow
 from webgazermodel import WebGazerModel
 
-dirToView = ["./P_1/1491423217564_2_-study-dot_test_instructions_frames/"]
+dirToTrain = [line.strip()+'/' for line in open('./train_1430_1.txt')]
+dirToTest = [line.strip()+'/' for line in open('./test_1430_1.txt')]
 sample_train_directory = "./P_1/1491423217564_2_-study-dot_test_instructions_frames/"
-sample_test_directory = "./P_1/1491423217564_22_-study-educational_advantages_of_social_networking_sites_instructions_frames/"
+sample_test_directory = "./P_1/1491423217564_29_-study-where_to_find_morel_mushrooms_writing_frames/"
 dataFile = "gazePredictions.csv"
 patchXSize = 40
 patchYSize = 15
@@ -20,12 +21,13 @@ patchYSize = 15
 class Gaze(RNGDataFlow):
 
   def __init__(self, dirs, dataFile, meta_dir=None, shuffle=None, dir_structure=None):
-    # self.imglist = []
+    self.imglist = []
     self.imgs = []
     self.labels = []
-    self.generate_data(dirs, dataFile)
+    for d in dirs:
+      self.generate_labels_and_img_list(d, dataFile)
 
-  def generate_data(self, directory, dataFile):
+  def generate_labels_and_img_list(self, directory, dataFile):
     with open( directory + dataFile ) as f:
       readCSV = csv.reader(f, delimiter=',')
       for row in readCSV:
@@ -36,55 +38,50 @@ class Gaze(RNGDataFlow):
         tobiiLeftEyeGazeY = float( row[3] )
         tobiiRightEyeGazeX = float( row[4] )
         tobiiRightEyeGazeY = float( row[5] )
-        webgazerX = float( row[6] )
-        webgazerY = float( row[7] )
         clmTracker = row[8:len(row)-1]
         clmTracker = [float(i) for i in clmTracker]
         clmTrackerInt = [int(i) for i in clmTracker]
-
         tobiiEyeGazeX = (tobiiLeftEyeGazeX + tobiiRightEyeGazeX) / 2
         tobiiEyeGazeY = (tobiiLeftEyeGazeY + tobiiRightEyeGazeY) / 2
 
-        img = cv2.imread( frameFilename )
-
-        # Middle of left eye
-        for i in range(54,56,2):
-          x = clmTrackerInt[i]
-          y = clmTrackerInt[i+1]
-          leftEye = img[y-(patchYSize-1)/2:y+(patchYSize-1)/2, x-(patchXSize/2):x+(patchXSize/2)-1]
-          # img = cv2.circle(img, (clmTrackerInt[i],clmTrackerInt[i+1]), 4, (255,0,0), -4 )
-
-        # Middle of right eye
-        for i in range(64,66,2):
-          x = clmTrackerInt[i]
-          y = clmTrackerInt[i+1]
-          rightEye = img[y-(patchYSize-1)/2:y+(patchYSize-1)/2, x-(patchXSize/2):x+(patchXSize/2)-1]
-          # img = cv2.circle(img, (clmTrackerInt[i],clmTrackerInt[i+1]), 4, (255,0,0), -4 )
-
-        combined = np.concatenate((leftEye, rightEye), axis=1)
-        combined = cv2.resize(combined, (80, 15))
-        self.imgs.append(combined)
+        left_eye_x = clmTrackerInt[54]
+        left_eye_y = clmTrackerInt[55]
+        right_eye_x = clmTrackerInt[64]
+        right_eye_y = clmTrackerInt[65]
+        self.imglist.append([frameFilename, left_eye_x, left_eye_y, right_eye_x, right_eye_y])
         # self.labels.append(np.array([tobiiEyeGazeX, tobiiEyeGazeY]))
-        self.labels.append(tobiiEyeGazeX)
+        self.labels.append(tobiiLeftEyeGazeX)
+
+  def generate_data(self, args):
+    frameFilename, left_eye_x, left_eye_y, right_eye_x, right_eye_y = args
+    img = cv2.imread(frameFilename)
+    # Middle of left eye
+    leftEye = img[left_eye_y-(patchYSize-1)/2:left_eye_y+(patchYSize-1)/2, left_eye_x-(patchXSize/2):left_eye_x+(patchXSize/2)-1]
+
+    # Middle of right eye
+    rightEye = img[right_eye_y-(patchYSize-1)/2:right_eye_y+(patchYSize-1)/2, right_eye_x-(patchXSize/2):right_eye_x+(patchXSize/2)-1]
+
+    leftEye = cv2.resize(leftEye, (40, 15))
+    return leftEye
 
 
   def get_data(self):
-    for k in np.arange(len(self.imgs)):
-      yield [self.imgs[k], self.labels[k]]
+    for k in np.arange(len(self.imglist)):
+      yield [self.generate_data(self.imglist[k]), self.labels[k]]
 
   def size(self):
-    return len(self.imgs)
+    return len(self.imglist)
 
 def load_data(directory, dataFile):
   data = Gaze(directory, dataFile)
   # augmentators = []
   # data = AugmentImageComponent(data, augmentators)
-  data = BatchData(data, 1)
+  data = BatchData(data, 50)
   return data
 
 def main():
-  dataset_train = load_data(sample_train_directory, dataFile)
-  dataset_test = load_data(sample_test_directory, dataFile)
+  dataset_train = load_data(dirToTrain, dataFile)
+  dataset_test = load_data(dirToTest, dataFile)
   config = TrainConfig(
     model = WebGazerModel(),
     dataflow = dataset_train,
